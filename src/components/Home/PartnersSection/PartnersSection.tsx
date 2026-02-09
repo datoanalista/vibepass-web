4b5d3781629bb93e8de1d64f845fad196db76608
Diego Ignacio Pino Araya <diego.pino@MacBook-Pro-de-Administrador-3.local>
2026-02-09 00:55:13 -0300

    add logo carrousel

--- src/components/Home/PartnersSection/PartnersSection.tsx
+++ src/components/Home/PartnersSection/PartnersSection.tsx
@@ -3,14 +3,51 @@ import { getImagePath } from '@/utils/getImagePath';
 import styles from './PartnersSection.module.css';
 
 const PartnersSection: React.FC = () => {
+  const carouselLogos = [
+    "Colloky.png",
+    "DBS.png",
+    "NESQUICK.png",
+    "alemana.png",
+    "ansaldo.png",
+    "arauco.png",
+    "azaleia.png",
+    "bilzYPap.png",
+    "ccu.png",
+    "chevrolet.png",
+    "clinicaMds.png",
+    "cocaCola.png",
+    "crush2.png",
+    "enap.png",
+    "fanta.png",
+    "gourmet2.png",
+    "gtd.png",
+    "guallarauco.png",
+    "hyundai.png",
+    "inmobilariaSecurity.png",
+    "kn.png",
+    "krispyKrem.png",
+    "laPreferida.png",
+    "lipton.png",
+    "littleCesars.png",
+    "marley.png",
+    "masisa.png",
+    "milo.png",
+    "nutrisco.png",
+    "papaJhons.png",
+    "schoolOfRock.png",
+    "scotiabank.png",
+    "unimarc.png",
+    "vendomatica2.png",
+  ];
+
   return (
     <section className={styles.partnersSection}>
       <div className={styles.container}>
-        <h2 className={styles.title}>
-          ELLOS CONFÍAN EN NOSOTROS
-        </h2>
-        
-        <div className={styles.decorativeBar}></div>
+        <img
+          src={getImagePath("/images/ourCustomers.png")}
+          alt="Nuestros clientes"
+          className={styles.titleImage}
+        />
         
         <div className={styles.logosContainer}>
           <img 
@@ -18,6 +55,23 @@ const PartnersSection: React.FC = () => {
             alt="Empresas que confían en nosotros" 
             className={styles.logosImage}
           />
+        </div>
+
+        <div className={styles.carouselSection}>
+          <div className={styles.carouselTitle}>CONFÍAN EN NOSOTROS</div>
+          <div className={styles.carouselViewport}>
+            <div className={styles.carouselTrack}>
+              {[...carouselLogos, ...carouselLogos].map((logo, index) => (
+                <img
+                  key={`${logo}-${index}`}
+                  src={getImagePath(`/images/imagenesK/${logo}`)}
+                  alt=""
+                  aria-hidden="true"
+                  className={styles.carouselLogo}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
